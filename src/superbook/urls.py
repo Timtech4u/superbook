ed1b21b562fa2ca47ef1afdd16663aff3b89860e
Olaleke Timothy <timothyolaleke@gmail.com>
2017-02-01 20:00:09 +0000

    Superbook with Django1.10

---
+++ src/superbook/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from profiles.views import SignInAndSignUp, LogoutView, AboutView
+from posts.views import MyFeedView
+
+urlpatterns = [
+    url(r'^$', SignInAndSignUp.as_view(template_name='home.html'),
+        name='home'),
+    url(r'^about/$', AboutView.as_view(),
+        name='about'),
+    url(r'^myfeed/$', MyFeedView.as_view(),
+        name='myfeed'),
+    url(r'^accounts/logout$', LogoutView.as_view(),
+        name='logout'),
+
+    url(r'^admin/', include(admin.site.urls)),
+]
